fa82305c4a8af1d8abbae5a5c59aea6bfafa3ebe
pnemrow <pnemrow@gmail.com>
2015-05-06 20:48:20 -0600

    Laid groundwork for editable user info
    Created form that is editable. Toggles the the form to an editable
    state and back

--- client/app/components/user_dashboard/dashboard.js
+++ client/app/components/user_dashboard/dashboard.js
@@ -3,13 +3,49 @@ import Router, {RouteHandler} from 'react-router'
 import {Link} from 'react-router'
 
 export default React.createClass({
+  getInitialState(){
+    return {
+      editstatus: true,
+      username: 'pnemrow',
+      email: 'pnemrow@gmail.com',
+      disabled: true,
+      editHide: false,
+      saveHide: true
+    }
+  },
+  toggleEdit(e){
+    e.preventDefault();
+    if(this.state.editstatus == true){
+      this.setState({
+        disabled: false,
+        editHide: true,
+        saveHide: false,
+        editstatus: false
+      })
+      React.findDOMNode(this.refs.usernameInput).focus()
+    }
+    else{
+      this.setState({
+        disabled: true,
+        editHide: false,
+        saveHide: true,
+        editstatus: true
+      })
+    }
+  },
+
   render: function(){
     return(
       <div>
         <h1>This is the user dashboard</h1>
-        <p>UserName: </p>
-        <p>Email: </p>
-        <button className="user_info-edit_button">Edit Information</button>
+        <form >
+          Username:
+          <input type="text" ref="usernameInput" className="user_dashboard-username" defaultValue={this.state.username} disabled = {this.state.disabled} />
+          Email:
+          <input type="text" className="user_dashboard-email" defaultValue={this.state.email} disabled = {this.state.disabled} /> 
+          <button className="user_dashboard-info_edit_button" onClick={this.toggleEdit} hidden={this.state.editHide}>Edit Information</button>
+          <button className="user_dashboard-info_save_button" onClick={this.toggleEdit} hidden={this.state.saveHide} >Save</button>
+        </form>
         <p><Link className="add_business-button" to="profile_builder" > Add a Business </Link></p>
       </div>
     )
